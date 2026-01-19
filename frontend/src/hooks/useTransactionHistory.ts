a5b8e617e9af6b69c185d6cd324083acaeeca492
morelucks <luckykamshak@gmail.com>
2026-01-19 22:46:42 +0100

    Add pagination to Recent Activity transaction history

--- frontend/src/hooks/useTransactionHistory.ts
+++ frontend/src/hooks/useTransactionHistory.ts
@@ -83,7 +83,7 @@ export const useTransactionHistory = () => {
       updateLoadingState();
       try {
         const response = await fetch(
-          `https://api.hiro.so/extended/v1/address/${address}/transactions?limit=10`
+          `https://api.hiro.so/extended/v1/address/${address}/transactions?limit=50`
         );
         
         if (response.ok) {
