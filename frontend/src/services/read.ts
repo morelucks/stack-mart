2779ff83f334209d020addde71b29afb424d7497
morelucks <morelucks@users.noreply.github.com>
2026-02-24 03:13:13 +0100

    feat: add getSellerReputation read-only function

--- frontend/src/services/read.ts
+++ frontend/src/services/read.ts
@@ -28,3 +28,16 @@ export const getListing = async (listingId: number, senderAddress: string) => {
 
   return cvToJSON(result);
 };
+
+export const getSellerReputation = async (userAddress: string, senderAddress: string) => {
+  const result = await callReadOnlyFunction({
+    network: getNetwork(),
+    contractAddress: CONTRACT_ADDRESS,
+    contractName: CONTRACT_NAME,
+    functionName: 'get-seller-reputation',
+    functionArgs: [principalCV(userAddress)],
+    senderAddress
+  });
+
+  return cvToJSON(result);
+};
