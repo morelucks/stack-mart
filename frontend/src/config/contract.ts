e3aecbf7120e37e5ccf4e888f4e77d8ea246ba96
morelucks <morelucks@users.noreply.github.com>
2026-02-24 03:37:39 +0100

    config: add StackMart Tips token contract address

--- frontend/src/config/contract.ts
+++ frontend/src/config/contract.ts
@@ -1,3 +1,4 @@
 export const CONTRACT_ADDRESS = 'SP34MN3DMM07BNAWYJSHTS4B08T8JRVK8AT810X1B';
 export const CONTRACT_NAME = 'stack-mart';
+export const TOKEN_CONTRACT = 'stackmart-tips';
 export const NETWORK = 'mainnet';
