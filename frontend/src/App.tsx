29264a6b06d66dcb49d07e12315ec1c960cc0d5e
morelucks <morelucks@users.noreply.github.com>
2026-02-24 03:40:54 +0100

    feat: add /token route to App navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Connect } from '@stacks/connect-react';
 import { HomePage } from './pages/HomePage';
 import { ListingDetail } from './pages/ListingDetail';
+import { TokenPage } from './pages/TokenPage';
 import { Header } from './components/Header';
 import { userSession } from './utils/auth';
 
@@ -26,6 +27,7 @@ function App() {
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/listing/:id" element={<ListingDetail listingId={1} />} />
+            <Route path="/token" element={<TokenPage />} />
           </Routes>
         </div>
       </BrowserRouter>
