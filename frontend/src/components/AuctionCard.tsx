aadf031e7c132a5ca8be899075c08b98e39d91d8
morelucks <morelucks@users.noreply.github.com>
2026-02-24 03:31:14 +0100

    build: successful production build with @stacks integration

--- frontend/src/components/AuctionCard.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export const AuctionCard = ({ auction }: any) => (
-  <div className="card">
-    <h3>Auction for Listing #{auction.listingId}</h3>
-    <p>Reserve: {auction.reservePrice} STX</p>
-    <p>Highest Bid: {auction.highestBid || "None"}</p>
-    <button className="btn btn-primary">Place Bid</button>
-  </div>
-);
--- frontend/src/components/ListingFilters.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export const ListingFilters = () => (
-  <div className="filters"><input placeholder="Min Price" /></div>
-);
